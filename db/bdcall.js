c5a8f98c9de7430f436496ab0be4cb944f87f684
Nada Zaki <nadazaki019@gmail.com>
2023-01-19 22:08:36 +0200

    rest of APIs added

--- db/bdcall.js
+++ db/bdcall.js
@@ -7,24 +7,32 @@ function createBDcall(bdcall){
 
 
 function getAllBDcall(){
-    return knex("bdcall").select(); // add between brackets what you want to show 
+    return knex("bdcall").select("*"); // add between brackets what you want to show 
+};
+
+
+
+function SelectBDcall(id){
+  return knex("bdcall").where("call_id",id);
 };
 
 
 function deleteBDcall(id){
-    return knex("bdcall").select("id",id).del();
+    return knex("bdcall").where("call_id",id).del();
 };
 
 function updateBDcall(id,bdcall){
-    return knex("bdcall").where("id",id).update(bdcall);
+    return knex("bdcall").where("call_id",id).update(bdcall);
 };
 
 
 module.exports= {
    createBDcall,
    getAllBDcall,
+   SelectBDcall,
    deleteBDcall,
-   updateBDcall
+   updateBDcall,
+   
 
 }
 
--- server.js
+++ server.js
@@ -1,46 +1,64 @@
 const express = require("express");
 const app = express();
-
+const bodyParser = require('body-parser');
 const db = require("./db/bdcall");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-const courses = [
-    { id: 1 , name : 'course1'},
-    { id: 2 , name : 'course2'},
-    { id: 3 , name : 'course3'},
-];
+
 
 app.get('/', (req,res) => { // where '/' refers to the home page of the website 
     res.send('This is the home page');
 });
 
-app.get('/api/courses1', (req,res) => {
-    res.send(courses);
-});
 
-app.post('/api/courses', (req,res) => {
-    const new_course = {
-        id: courses.length +1,
-        name: req.body.name
 
-    };
-    courses.push(new_course);
-    res.send(new_course);
-
-});
-
-app.post("/bdcall1", async (req,res) => {
-    const results =await db.createBDcall(req.body);
-    res.status(201).json({id: results[0]});
-});
-
-app.get("/bdcall2", async (req,res) => {
+app.get("/bdcall", async (req,res) => {
     const call =await db.getAllBDcall();
-    res.status(200).json({id: call });
+    //res.status(200).json({id : call});
+    res.send(call);
 });
 
+app.post("/test", async (req,res) =>{
+    
+    res.send("this post reqquest ir actually working ");
+});
+
+app.post("/bdcall", async (req,res) =>{
+    const results =  await db.createBDcall(req.body);
+    res.status(201).json({id: results[0]});
+    //res.send("this post reqquest ir actually working ");
+});
+
+app.patch("/bdcall/:id", async (req,res) =>{  //where id here refers to call_id ggiven by me not the database id or numberin 
+    console.log(req.params.id);
+    const carid =  await db.updateBDcall(req.params.id,req.body);
+    res.send("updated");
+    //res.status(200).json({ carid });
+    
+});
+
+app.delete("/bdcall/:id", async (req,res) =>{
+    console.log(req.params.id);
+    await db.deleteBDcall(req.params.id);
+    //res.send("deleted");
+    res.status(200).json({ success:true });
+    
+});
+
+
+app.get("/bdcall/:id", async (req,res) => {
+    const row =await db.SelectBDcall(req.params.id);
+    res.send(row);
+});
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`server is running on port ${port}`));
+
+
+
+
+
